8753ecce43d83d696421dc08a25b45d7cf6e6320
lucas-chen <lc2616@nyu.edu>
2015-02-09 17:02:26 -0500

    hw1 is done. except the program crashes at end

--- project_template/Windows/NYUCodebase/NYUCodebase/hw1.cpp
+++ project_template/Windows/NYUCodebase/NYUCodebase/hw1.cpp
@@ -2,7 +2,6 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-
 SDL_Window* displayWindow;
 
 
@@ -41,6 +40,46 @@ void DrawSprite(GLint texture, float x, float y, float rotation) {
 	glDisable(GL_TEXTURE_2D);
 }
 
+void DrawPortal(GLint texture, float x, float y, float rotation) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0);
+	glScalef(0.25, 0.25, 0.0);
+	glRotatef(rotation, 0.0, 0.0, 1.0);
+	GLfloat quad[] = { -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f };
+	glVertexPointer(2, GL_FLOAT, 0, quad);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	GLfloat quadUVs[] = { 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0 };
+	glTexCoordPointer(2, GL_FLOAT, 0, quadUVs);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDrawArrays(GL_QUADS, 0, 4);
+	glDisable(GL_TEXTURE_2D);
+}
+
+void DrawCharacter(GLint texture, float x, float y, float rotation) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0);
+	glScalef(0.1, 0.1, 0.0);
+	glRotatef(rotation, 0.0, 0.0, 1.0);
+	GLfloat quad[] = { -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f };
+	glVertexPointer(2, GL_FLOAT, 0, quad);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	GLfloat quadUVs[] = { 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0 };
+	glTexCoordPointer(2, GL_FLOAT, 0, quadUVs);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDrawArrays(GL_QUADS, 0, 4);
+	glDisable(GL_TEXTURE_2D);
+}
+
 
 
 int main(int argc, char *argv[])
@@ -52,49 +91,72 @@ int main(int argc, char *argv[])
 
 	bool done = false;
 	
-
-
 	SDL_Event event;
-
 	
-
+	
+	glMatrixMode(GL_PROJECTION);
+	glViewport(0, 0, 800, 600);
+	glOrtho(-1.33, 1.33, -1.0, 1.0, -1.0, 1.0);
 
 	while (!done) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
 			}
-			
-
-
-
-
-
-
-			//   screen blinks and sprite smears when rotating
-
-			GLuint plane = LoadTexture("hw1/aliens.png");
-
-
-
-
-			float lastFrameTicks = 0.0f;
-			float ticks = (float)SDL_GetTicks() / 100.0f;
-			float elapsed = ticks - lastFrameTicks;
-			lastFrameTicks = ticks;
-
-			lastFrameTicks += elapsed;
-			DrawSprite(plane, 0.0, 0.0, lastFrameTicks);
-
-			//
-
-
-
 		}
+
+
+
+		
+		glMatrixMode(GL_MODELVIEW);
+
+		glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		
+		float lastFrameTicks = 0.0f;
+		float ticks = (float)SDL_GetTicks() / 10.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+		lastFrameTicks += elapsed;
+
+		
+		GLuint picture1 = LoadTexture("hw1/chipWhite_side.png");
+		GLuint picture2 = LoadTexture("hw1/rpgTile108.png");
+		GLuint picture3 = LoadTexture("hw1/snail.png");
+
+		DrawPortal(picture1, 0.8, 0.8, 0.0);
+		
+		glLoadIdentity();
+		
+		glTranslatef(-0.2, -0.2, 0.0);
+		glScalef(0.5, 0.5, 0.5);
+		GLfloat triangle[] = { -0.5f, 0.0f, -1.0f, -1.0f, 0.0f, -1.0f };
+		glVertexPointer(2, GL_FLOAT, 0, triangle);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		GLfloat triangleColors[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+			0.0, 0.0 };
+		glColorPointer(3, GL_FLOAT, 0, triangleColors);
+		glEnableClientState(GL_COLOR_ARRAY);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableClientState(GL_COLOR_ARRAY);
+
+		glLoadIdentity();
+
+		DrawPortal(picture2, -1.0, -0.8, 0.0);
+
+		glLoadIdentity();
+
+		DrawCharacter(picture3, 0.8 + (lastFrameTicks/-10000.0), 0.8 + (lastFrameTicks/-10000.0), 0.0);
+
+		glLoadIdentity();
 
 		SDL_GL_SwapWindow(displayWindow);
 
 	}
+
+
+
 
 	SDL_Quit();
 	return 0;
